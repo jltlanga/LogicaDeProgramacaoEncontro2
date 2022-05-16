42418371d4f052a85dea4352f5e11f9904c92153
jltlanga <julitolanga@gmail.com>
2022-05-16 01:06:57 -0300

    Commit do exercicio03 do encontro2

---
+++ ex03ParImpar.js
@@ -0,0 +1,15 @@
+let entrada = require('readline-sync');
+let n = parseFloat(entrada.question('Informe um número:' ))
+for(i = 0; i<=n; i++){
+    if(i == 0){
+        console.log('Zero ' + i);
+    }else{
+        if(i % 2 != 0){
+            console.log('Ímpar ' + i);
+        }else{
+            console.log('Par ' + i);
+        }
+
+    }
+    
+}
